5b2f05a5d6c64c913ed4bebc08ae9955d2af45ee
webfrontier-niko <n_takahashi@webfrontier.co.jp>
2026-06-26 14:37:11 +0900

    feat: add codegen for developers 今後型定義を一つのコマンドで生成できるようにcodegenを追加

---
+++ apps/web/src/gql/graphql.ts
@@ -0,0 +1,62 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type AuthResponse = {
+  __typename?: 'AuthResponse';
+  accessToken: Scalars['String']['output'];
+  displayName: Scalars['String']['output'];
+  email: Scalars['String']['output'];
+};
+
+export type LoginDto = {
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  login: AuthResponse;
+  signUp: SignUpResponse;
+};
+
+
+export type MutationLoginArgs = {
+  dto: LoginDto;
+};
+
+
+export type MutationSignUpArgs = {
+  dto: SignUpDto;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  /** フロント→BFF→gRPCサービスの疎通確認用クエリ */
+  health: Scalars['String']['output'];
+};
+
+export type SignUpDto = {
+  displayName: Scalars['String']['input'];
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+};
+
+export type SignUpResponse = {
+  __typename?: 'SignUpResponse';
+  email: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+};
